0926f0dd314058474f205a6cfad3787b64562899
John van Dijk <john@rubylibre.com>
2014-10-02 12:09:48 +0200

    We use IBAN now

--- lib/active_merchant/billing/targetpay_direct_debit.rb
+++ lib/active_merchant/billing/targetpay_direct_debit.rb
@@ -3,22 +3,22 @@ require 'digest/sha1'
 module ActiveMerchant
   module Billing
     class TargetpayDirectDebitGateway < Gateway
-      
+
       # url will be completed below
       URL = "https://www.targetpay.com/directdebit/"
-      
+
       def initialize(options = {})
         requires!(options, :rtlo, :salt)
         @options = options
         super
       end
-      
+
       def setup_purchase(money, options)
         requires!(options, :cbank, :cname, :description, :reporturl, :retrymax, :securitylevel, :userip)
-        
+
         raise ArgumentError.new("money should be >= EUR 1,00")                  if money < 100
         raise ArgumentError.new("money should be <= EUR 1000,00")               if money > 100000
-        raise ArgumentError.new("cbank should =~ /[0-9]+/")                     if !(options[:cbank] =~ /^[0-9]+$/)
+        # raise ArgumentError.new("cbank should =~ /[0-9]+/")                     if !(options[:cbank] =~ /^[0-9]+$/)
         raise ArgumentError.new("cname is blank")                               if options[:cname].blank?
         raise ArgumentError.new("description should =~ /^[0-9A-Z\ ]{1,32}$/i")  if !(options[:description] =~ /^[0-9A-Z\ ]{1,32}$/i)
         raise ArgumentError.new("reporturl is blank")                           if options[:reporturl].blank?
@@ -41,7 +41,7 @@ module ActiveMerchant
           :userip         => options[:userip]
         }))
       end
-      
+
       def details_for(token)
         build_check_response(commit('check', {
           :checksum => Digest::MD5.hexdigest(token + @options[:rtlo] + @options[:salt]),
@@ -50,9 +50,9 @@ module ActiveMerchant
           :trxid    => token
         }))
       end
-      
+
       private
-      
+
       def commit(action, params)
         url   = URL + action + "?#{params.collect { |k,v| "#{k}=#{v}" }.join("&") }"
         uri   = URI.parse(url)
@@ -60,7 +60,7 @@ module ActiveMerchant
         http.use_ssl = (uri.scheme == 'https')
         http.get(uri.request_uri).body
       end
-      
+
       def build_start_response(response)
         params = {}
         message = response
@@ -72,15 +72,15 @@ module ActiveMerchant
         end
         TargetpayDirectDebitStartResponse.new(success, message, params)
       end
-      
+
       def build_check_response(response)
         message = response
         success = false
         if response[0..5] == "000000"
           success = true
-        end        
+        end
         TargetpayDirectDebitCheckResponse.new(success, message)
-      end      
+      end
     end
   end
 end
